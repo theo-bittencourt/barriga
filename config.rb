1ac14761bc3c8a69c2ee81e9078a8919bf32aadd
Theo Bittencourt <theodrums@gmail.com>
2014-07-15 12:35:09 -0300

    Build com path relativos

--- config.rb
+++ config.rb
@@ -1,19 +1,13 @@
-require "compass"
-require "bootstrap-sass"
 
-### 
-# Compass 
+###
+# Compass
 
 http_images_path = "source/images/"
 
 activate :directory_indexes
-
-
 ###
-
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
+# Compass
+###
 
 # Change Compass configuration
 # compass_config do |config|
@@ -21,45 +15,37 @@ activate :directory_indexes
 # end
 
 ###
-# Haml
-###
-
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-###
-# Page command
+# Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
 ###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -68,34 +54,24 @@ activate :directory_indexes
 #   end
 # end
 
-# Change the CSS directory
-# set :css_dir, "rra/stylesheets"
-
-# Change the JS directory
-# set :js_dir, "rra/javascripts"
-
-# Change the images directory
-# set :images_dir, "rra/images"
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # Enable cache buster
-  # activate :cache_buster
-  
+  # activate :asset_hash
+
   # Use relative URLs
   # activate :relative_assets
-  
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-  
+
   # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # set :http_prefix, "/Content/images/"
 end
